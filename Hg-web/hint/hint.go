873d5208959aa317ca53061606398b4a35fa9152
hhgnbz <chch_cy@163.com>
2023-06-27 13:42:20 +0800

    feat:group control

--- Hg-web/hint/hint.go
+++ Hg-web/hint/hint.go
@@ -7,22 +7,58 @@ import (
 // handlerFunc for users define methods and actions of request path
 type handlerFunc func(c *Context)
 
+// 路由分组
+// e.g.
+// 以/post开头的路由匿名可访问。
+// 以/admin开头的路由需要鉴权。
+// 以/api开头的路由是 RESTful 接口，可以对接第三方平台，需要三方平台鉴权。
+// 实现的分组控制也是以前缀来区分，并且支持分组的嵌套。
+// e.g.
+// /post是一个分组，/post/a和/post/b可以是该分组下的子分组。
+// 作用在/post分组上的中间件(middleware)，也都会作用在子分组，子分组还可以应用自己特有的中间件。
+
+type RouterGroup struct {
+	prefix      string
+	parent      *RouterGroup // 支持分组嵌套
+	middlewares []handlerFunc
+	engine      *Engine // 所有分组共享一个Engine，保存一个指针方便通过Engine访问其他接口
+}
+
 // Engine implements interface named ServeHTTP
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup // 保存所有分组，所有路由操作通过分组实现
 }
 
 // New is the constructor of Engine for users
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
+}
+
+// Group is defined to create a new RouterGroup
+// remember all groups share the same Engine instance
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	e := group.engine
+	newGroup := &RouterGroup{
+		engine: e,
+		prefix: group.prefix + prefix,
+		parent: group,
+	}
+	e.groups = append(e.groups, newGroup)
+	return newGroup
 }
 
 // inside func for users to add router
 // m -> http method(get/post)
 // p -> path
 // h -> handler func
-func (e *Engine) addRouter(m string, p string, h handlerFunc) {
-	e.router.addRouter(m, p, h)
+func (group *RouterGroup) addRouter(m string, p string, h handlerFunc) {
+	pattern := group.prefix + p
+	group.engine.router.addRouter(m, pattern, h)
 }
 
 // Run is a method for users to run the server on appoint port
@@ -31,13 +67,13 @@ func (e *Engine) Run(port string) error {
 }
 
 // GET is a method for users to add "get" router
-func (e *Engine) GET(p string, h handlerFunc) {
-	e.addRouter("GET", p, h)
+func (group *RouterGroup) GET(p string, h handlerFunc) {
+	group.addRouter("GET", p, h)
 }
 
 // POST is a method for users to add "post" router
-func (e *Engine) POST(p string, h handlerFunc) {
-	e.addRouter("POST", p, h)
+func (group *RouterGroup) POST(p string, h handlerFunc) {
+	group.addRouter("POST", p, h)
 }
 
 // impl interface named ServeHTTP
--- Hg-web/main.go
+++ Hg-web/main.go
@@ -7,23 +7,34 @@ import (
 
 func main() {
 	r := hint.New()
-	r.GET("/", func(c *hint.Context) {
-		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
+	r.GET("/index", func(c *hint.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
 	})
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/", func(c *hint.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello Hint</h1>")
+		})
 
-	r.GET("/hello", func(c *hint.Context) {
-		// expect /hello?name=hb
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+		v1.GET("/hello", func(c *hint.Context) {
+			// expect /hello?name=hg
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *hint.Context) {
+			// expect /hello/hg
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *hint.Context) {
+			c.JSON(http.StatusOK, hint.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
 
-	r.GET("/hello/:name", func(c *hint.Context) {
-		// expect /hello/hb
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-
-	r.GET("/assets/*filepath", func(c *hint.Context) {
-		c.JSON(http.StatusOK, hint.H{"filepath": c.Param("filepath")})
-	})
+	}
 
 	r.Run(":9999")
 }
